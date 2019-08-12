bf85df52218f2251a854c730cc073cc1441fafa3
ashwini <ashwini.jadhav@terralogic.com>
2019-08-12 12:47:23 +0530

    My First commit

---
+++ arithmetic-operations.java
@@ -0,0 +1,14 @@
+import java.lang.System;
+
+class Add
+{
+    public static void main(String args[])
+    {
+        int a =3, b=4 ,c;
+        c = a+b ;
+        System.out.println(c);
+
+    }
+
+
+}
