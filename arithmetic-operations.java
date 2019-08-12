99a89250e2e52c23748466d57f100d143f25349d
ashwini <ashwini.jadhav@terralogic.com>
2019-08-12 13:02:22 +0530

    My second commit from different repo

--- arithmetic-operations.java
+++ arithmetic-operations.java
@@ -4,10 +4,11 @@ class Add
 {
     public static void main(String args[])
     {
-        int a =3, b=4 ,c;
+        int a =3, b=4 ,c,d;
         c = a+b ;
+	d = b-a;
         System.out.println(c);
-
+	System.out.println(d);
     }
 
 
